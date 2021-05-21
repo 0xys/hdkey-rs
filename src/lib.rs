76012e5b464aed13fa64a0f8bc69f390b8a6e2f6
Yushi Sato <yushi.kinger@gmail.com>
2021-05-21 20:50:51 +0900

    test vector 2 and 3 passed

--- src/lib.rs
+++ src/lib.rs
@@ -111,4 +111,150 @@ mod tests {
         let bs58 = bs58.as_str();
         assert_eq!("xprvA41z7zogVVwxVSgdKUHDy1SKmdb533PjDz7J6N6mV6uS3ze1ai8FHa8kmHScGpWmj4WggLyQjgPie1rFSruoUihUZREPSL39UNdE3BBDu76", bs58);
     }
+
+    /// https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#test-vector-2
+    #[test]
+    fn test_vector_2() {
+        let seed_hex_str = "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542";
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = ExtendedPrivateKey::from_seed_hex(seed_hex_str).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U", bs58);
+
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m/0
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = xpriv.derive_child(0).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprv9vHkqa6EV4sPZHYqZznhT2NPtPCjKuDKGY38FBWLvgaDx45zo9WQRUT3dKYnjwih2yJD9mkrocEZXo1ex8G81dwSM1fwqWpWkeS3v86pgKt", bs58);
+
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m/0/2147483647'
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = xpriv.derive_hardended_child(2147483647).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub6ASAVgeehLbnwdqV6UKMHVzgqAG8Gr6riv3Fxxpj8ksbH9ebxaEyBLZ85ySDhKiLDBrQSARLq1uNRts8RuJiHjaDMBU4Zn9h8LZNnBC5y4a", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprv9wSp6B7kry3Vj9m1zSnLvN3xH8RdsPP1Mh7fAaR7aRLcQMKTR2vidYEeEg2mUCTAwCd6vnxVrcjfy2kRgVsFawNzmjuHc2YmYRmagcEPdU9", bs58);
+
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m/0/2147483647'/1
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = xpriv.derive_child(1).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub6DF8uhdarytz3FWdA8TvFSvvAh8dP3283MY7p2V4SeE2wyWmG5mg5EwVvmdMVCQcoNJxGoWaU9DCWh89LojfZ537wTfunKau47EL2dhHKon", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprv9zFnWC6h2cLgpmSA46vutJzBcfJ8yaJGg8cX1e5StJh45BBciYTRXSd25UEPVuesF9yog62tGAQtHjXajPPdbRCHuWS6T8XA2ECKADdw4Ef", bs58);
+
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m/0/2147483647'/1/2147483646'
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = xpriv.derive_hardended_child(2147483646).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprvA1RpRA33e1JQ7ifknakTFpgNXPmW2YvmhqLQYMmrj4xJXXWYpDPS3xz7iAxn8L39njGVyuoseXzU6rcxFLJ8HFsTjSyQbLYnMpCqE2VbFWc", bs58);
+
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m/0/2147483647'/1/2147483646'/2
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = xpriv.derive_child(2).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j", bs58);
+    }
+
+    /// https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#test-vector-3
+    #[test]
+    fn test_vector_3(){
+        let seed_hex_str = "4b381541583be4423346c643850da4b320e46a87ae3d2a4e6da11eba819cd4acba45d239319ac14f863b8d5ab5a0d0c64d2e8a1e7d1457df2e5a3c51c73235be";
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = ExtendedPrivateKey::from_seed_hex(seed_hex_str).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub661MyMwAqRbcEZVB4dScxMAdx6d4nFc9nvyvH3v4gJL378CSRZiYmhRoP7mBy6gSPSCYk6SzXPTf3ND1cZAceL7SfJ1Z3GC8vBgp2epUt13", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprv9s21ZrQH143K25QhxbucbDDuQ4naNntJRi4KUfWT7xo4EKsHt2QJDu7KXp1A3u7Bi1j8ph3EGsZ9Xvz9dGuVrtHHs7pXeTzjuxBrCmmhgC6", bs58);
+
+
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        //  m/0'
+        //  --------------------------------------------------------------------------------------------------------------------------------
+        let xpriv = xpriv.derive_hardended_child(0).unwrap();
+        let xpub = xpriv.to_x_pub();
+
+        // xpub
+        let bs58 = xpub.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xpub68NZiKmJWnxxS6aaHmn81bvJeTESw724CRDs6HbuccFQN9Ku14VQrADWgqbhhTHBaohPX4CjNLf9fq9MYo6oDaPPLPxSb7gwQN3ih19Zm4Y", bs58);
+
+        // xpriv
+        let bs58 = xpriv.to_base58();
+        let bs58 = bs58.as_str();
+        assert_eq!("xprv9uPDJpEQgRQfDcW7BkF7eTya6RPxXeJCqCJGHuCJ4GiRVLzkTXBAJMu2qaMWPrS7AANYqdq6vcBcBUdJCVVFceUvJFjaPdGZ2y9WACViL4L", bs58);
+    }
 }
